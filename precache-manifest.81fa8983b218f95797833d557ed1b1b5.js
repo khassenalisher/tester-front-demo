5d5b17f1b62e8dae28f984939ac180246d7ea429
khassenalisher <khassenalisher@gmail.com>
2020-01-17 16:26:42 +0600

    Updates

---
+++ precache-manifest.81fa8983b218f95797833d557ed1b1b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f825077e3789f2e78b32a422c5cec35d",
+    "url": "/tester-front-demo/index.html"
+  },
+  {
+    "revision": "c69e0b74a2b622aa9936",
+    "url": "/tester-front-demo/static/css/main.39b92ea4.chunk.css"
+  },
+  {
+    "revision": "2de00af9ef9e60fd5c5b",
+    "url": "/tester-front-demo/static/js/2.22875b9a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tester-front-demo/static/js/2.22875b9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c69e0b74a2b622aa9936",
+    "url": "/tester-front-demo/static/js/main.83e71ddd.chunk.js"
+  },
+  {
+    "revision": "ebac7bfacf5f9f0a8c4d",
+    "url": "/tester-front-demo/static/js/runtime-main.622c1761.js"
+  },
+  {
+    "revision": "5267aa23b2e3774bdcc80d19353753a3",
+    "url": "/tester-front-demo/static/media/arrow-image.5267aa23.svg"
+  },
+  {
+    "revision": "08e3601cb29747029e7b34c8fcb8d5ca",
+    "url": "/tester-front-demo/static/media/company-logo.08e3601c.svg"
+  }
+]);
